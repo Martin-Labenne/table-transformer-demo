f558b26aa13122e185ccf5b6f945eb7cf4005145
Martin-Labenne <martinlabenne@gmail.com>
2024-10-17 15:15:05 +0200

    feat: pdf token reader

--- src/tokenExtraction.py
+++ src/tokenExtraction.py
@@ -22,12 +22,39 @@ class TokenReader(Reader):
                 ],
                 'text': text,
                 'proba': proba,
+                'block_num': 0, 
+                'line_num': 0, 
                 'span_num': i,
-                'line_num': 0,
-                'block_num': 0
             }
             for i, (bbox_coord, text, proba) in enumerate(reader_results)
         ] 
 
         return tokens
 
+class PDFTokenReader(): 
+    def __init__(self, flags=None, dpi=100):
+        self.flags = flags
+        self.default_pdf_dpi = 72
+        self.dpi = dpi
+
+    def get_tokens(self, page): 
+        words = page.get_text(option="words", flags=self.flags)
+        # make sure the bounding boxes are in the same scale as the generated image
+        dpi_scale = self.dpi/self.default_pdf_dpi
+        tokens = []
+        for word_meta in words:
+            pdf_scaled_word_bbox = [ word_meta[0], word_meta[1], word_meta[2], word_meta[3] ]
+            tokens.append({
+                'bbox': [ 
+                    word_cord * dpi_scale 
+                    for word_cord in pdf_scaled_word_bbox
+                ], 
+                'text': word_meta[4],
+                'proba': 1, 
+                # 'flags': 0,
+                'block_num': word_meta[5],
+                'line_num': word_meta[6],
+                'span_num': word_meta[7]
+            })    
+        
+        return tokens
